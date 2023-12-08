e21b39b62a9f5fe7a65e1b9a34b3c165db034e4f
Harshit <gharshit89@gmail.com>
2023-12-09 00:13:16 +0530

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import Group from './components/Group/Group';
+import Navbar from './components/Navbar/Navbar';
+import { FaExclamationTriangle, FaAdn } from 'react-icons/fa';
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
+import { FcTodoList, FcApproval, FcLowPriority, FcMediumPriority, FcHighPriority, FcCancel, FcPieChart, FcExpired, FcBusinessman, FcManager, FcServiceMark, FcLinux } from "react-icons/fc";
 import './App.css';
-import Kanban from './components/Kanban';
-import Title from './components/Title';
 
 function App() {
 

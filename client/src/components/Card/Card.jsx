958995bd7dd01ac9cd9f63b6a8fc86cf076d4cc8
Harshit <gharshit89@gmail.com>
2023-12-09 10:11:43 +0530

    updated card

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Card.css";
+
+
+function Card({ ticket, group }) {
+    return (
+        <div className="card">
+            <div className="details ">
+                <div>
+                    <span className="light-font">{ticket.id}</span>
+                </div>
+                <div className="col-2">
+                    {group !== "status" && (
+                        <span>
+                            {ticket.statusIcon &&
+                                React.createElement(ticket.statusIcon, {
+                                    className: "icons-class",
+                                })}
+                        </span>
+                    )}
+                    <span>{ticket.title}</span>
+                </div>
+                <div className="col-3">
+                    {group !== "priority" && (
+                        <span className="col-3-icon">
+                            {ticket.priorityIcon &&
+                                React.createElement(ticket.priorityIcon, {
+                                    className: "icons-class",
+                                })}
+                        </span>
+                    )}
+                    <span className="light-font">{ticket.tag}</span>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Card;
